c3f950f078aea5124c4831c3f9ba4189e7cf5072
Chiyomi Arima <>
2020-05-09 15:45:26 +0900

    SP完成

--- pages/index.vue
+++ pages/index.vue
@@ -1,63 +1,52 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title red">me-plus</h1>
-      <h2 class="subtitle">My splendid Nuxt.js project</h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
+  <div>
+    <main>
+      <figure>
+        <img src="~/assets/img/kv_main.jpg" alt="kv_main" />
+      </figure>
+      <p class="center">好きを仕事に</p>
+      <div class="scroll center">
+        <p class="montserrat">Scroll</p>
+        <img src="~/assets/img/arrow.svg" alt="arrow" />
       </div>
-      <p>Static Branch</p>
-    </div>
+    </main>
+    <section id="about">
+      <div class="inner">
+        <h1 class="montserrat black center">WE ARE</h1>
+        <div class="logo center">
+          <p>プログラミング女子会</p>
+          <img src="~/assets/img/logo.svg" alt="logo" />
+        </div>
+        <p class="center">
+          女性のためのプログラミング
+          <br />＆Webデザイン学習コミュニティ
+        </p>
+      </div>
+      <figure>
+        <img src="~/assets/img/kv_about.jpg" alt="kv_about" />
+      </figure>
+    </section>
+    <section id="joinus">
+      <div class="inner">
+        <h1 class="montserrat black center">SLACK</h1>
+        <div class="btn center">
+          <a
+            href="https://join.slack.com/t/me-plus-girls/shared_invite/zt-6d958wqo-r0a9idLVmXm4PYArNCXudQ"
+          >参加はこちら</a>
+        </div>
+      </div>
+    </section>
+    <section id="contact">
+      <div class="inner">
+        <h1 class="montserrat black center">CONTACT</h1>
+        <p class="center">お気軽にご連絡ください</p>
+        <form>
+          <input class="email" type="email" placeholder="メールアドレス" />
+          <input class="name" type="text" placeholder="お名前" />
+          <textarea placeholder="お問い合わせ内容"></textarea>
+          <input type="button" value="送信する" />
+        </form>
+      </div>
+    </section>
   </div>
 </template>
-
-<script>
-import Logo from "~/components/Logo.vue";
-
-export default {
-  components: {
-    Logo
-  }
-};
-</script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
-    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
-
-<style lang="scss">
-// .red {
-//   color: red;
-// }
-</style>
